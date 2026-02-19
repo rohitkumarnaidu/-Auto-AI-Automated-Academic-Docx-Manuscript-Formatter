e4aece51d8a5b8015e2656ab5091c5c5cfbe78b8
Bappadala Rohith Kumar Naidu <198095685+rohitkumarnaidu@users.noreply.github.com>
2026-02-19 23:53:23 +0530

    Remove moved files from root directory

--- debug_imports_v2.py
+++
@@ -1,7 +0,0 @@
-import sys
-import os
-sys.path.append("backend")
-
-print("Importing Validator v3...", file=sys.stderr)
-from backend.app.pipeline.validation.validator_v3 import DocumentValidator
-print("Import Success", file=sys.stderr)
